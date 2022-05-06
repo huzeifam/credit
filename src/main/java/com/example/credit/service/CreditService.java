1379521f373167dae613062881b77f3f39ccf22b
huzeifam <huzeifa.marzouka@adesso.de>
2022-05-06 12:56:55 +0200

    docker-compose

--- src/main/java/com/example/credit/service/CreditService.java
+++ src/main/java/com/example/credit/service/CreditService.java
@@ -29,15 +29,15 @@ public class CreditService {
 
     public CreditResponse requestCredit(CreditResponse cred) {
 
-        boolean credits = restTemplate.getForObject("http://account:8085/api/accounts/numbers", List.class).contains(cred.getAccountNo());
-//      boolean credits = restTemplate.getForObject("http://localhost:8085/api/accounts/numbers", List.class).contains(cred.getAccountNo());
+//        boolean credits = restTemplate.getForObject("http://account:8085/api/accounts/numbers", List.class).contains(cred.getAccountNo());
+      boolean credits = restTemplate.getForObject("http://localhost:8085/api/accounts/numbers", List.class).contains(cred.getAccountNo());
 
 
         if (!credits) {
             return null;
         } else {
-            restTemplate.put("http://account:8085/api/accounts/"+cred.getAccountNo()+"/deposit/"+cred.getCreditAmount(),List.class);
-//            restTemplate.put("http://localhost:8085/api/accounts/" + cred.getAccountNo() + "/deposit/" + cred.getCreditAmount(), List.class);
+//            restTemplate.put("http://account:8085/api/accounts/"+cred.getAccountNo()+"/deposit/"+cred.getCreditAmount(),List.class);
+            restTemplate.put("http://localhost:8085/api/accounts/" + cred.getAccountNo() + "/deposit/" + cred.getCreditAmount(), List.class);
             return creditRepository.save(cred);
 
 
@@ -55,16 +55,16 @@ public class CreditService {
 
     public ResponseEntity<Object> payCredit(CreditResponse credit) {
 
-//        Double balance = restTemplate.getForObject("http://localhost:8085/api/accounts/"+credit.getAccountNo()+"/balance", Double.class);
-        Double balance = restTemplate.getForObject("http://account:8085/api/accounts/"+credit.getAccountNo()+"/balance", Double.class);
+        Double balance = restTemplate.getForObject("http://localhost:8085/api/accounts/"+credit.getAccountNo()+"/balance", Double.class);
+//        Double balance = restTemplate.getForObject("http://account:8085/api/accounts/"+credit.getAccountNo()+"/balance", Double.class);
         if ( balance < credit.getRates()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).body("Pay off credit failed. \n\n" +
                     "Rate: "+ credit.getRates()+"€\n" +
                     "Current balance of account ("+credit.getAccountNo()+"): "+balance+"€");
         }
         else {
-//            restTemplate.put("http://localhost:8085/api/accounts/"+credit.getAccountNo()+"/withdraw/"+credit.getRates(),List.class);
-            restTemplate.put("http://account:8085/api/accounts/" + credit.getAccountNo() + "/withdraw/" + credit.getRates(), List.class);
+            restTemplate.put("http://localhost:8085/api/accounts/"+credit.getAccountNo()+"/withdraw/"+credit.getRates(),List.class);
+//            restTemplate.put("http://account:8085/api/accounts/" + credit.getAccountNo() + "/withdraw/" + credit.getRates(), List.class);
             creditRepository.save(credit);
             return ResponseEntity.ok(findByCreditNo(credit.getCreditNo()));
         }
